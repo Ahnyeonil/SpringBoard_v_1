2080d42d3e2ef174731e5a0a54da4a7c026a7460
yeonil <ahnyi527@gmail.com>
2021-12-07 18:13:35 +0900

    first_commit

---
+++ src/test/java/com/ahn/board/SpringBoardV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahn.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoardV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
