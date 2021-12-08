c24ce1233ef93c60a2182510cc0b00cdea1de1a6
yeonil <ahnyi527@gmail.com>
2021-12-08 18:19:11 +0900

    commit_5_database

---
+++ src/main/java/com/ahn/board/domain/user/User.java
@@ -0,0 +1,49 @@
+package com.ahn.board.domain.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+// 어느 필드에 어떤 값을 채워야하는지 명확히 알 수 있기 때문에 실수가 나지 않는다.
+@Builder
+@AllArgsConstructor
+// Lombok 어노테이션으로 빈 생성자를 만들어준다.
+@NoArgsConstructor
+// 해당 클래스가 엔티티를 위한 클래스이며, 해당 클래스의 인스턴스들이 JPA로 관리되는 엔티티 객체라는 것을 의미한다. (테이블)
+@Entity
+
+// Setter가 없는 이유 - 해당 클래스의 인스턴스가 언제 어디서 변해야 하는지 코드상으로 확인이 불가함
+public class User {
+
+	// Primary Key
+	@Id
+	// PK를 자동으로 생성
+	// sequence, auto_increment
+	// application.properties - use-new-id-generate-mappings: false 로 설정
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	// 해당 필드가 컬럼이라는 것을 말함
+	// 다양한 속성 가능
+	@Column(nullable = false, length = 30)
+	private String username;
+	
+	@Column(nullable = false, length = 100)
+	private String password;
+	
+	@Column(nullable = false, length = 50)
+	private String email;
+	
+	@Column(nullable = false, length = 50)
+	private String nickname;
+	
+	// JPA로 DB에 저장할 때 Enum 값을 어떤 형태로 저장할지 결정
+	// 기본적으로 int로 저장
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Role role;
+}
